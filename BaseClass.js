bc0567924655bbe06f577369a59523f7e10fdfb8
ishika1312 <70836108+ishika1312@users.noreply.github.com>
2020-11-12 17:52:12 +0530

    Delete BaseClass.js

--- BaseClass.js
+++
@@ -1,9 +0,0 @@
-class BaseClass {
-    constructor(x , y, width, height, radius) {
-        var options ={
-            isStatic: true,
-            restitution: 0.3,
-            friction: 0.5,
-            density: 1.2
-        }
-}}
